40ee079b5387e160a86882bc6d4a2454db12f27e
Nathan de Souza Miranda <nathansm2005@gmail.com>
2024-08-28 15:58:27 -0300

    commit estudo de C#

---
+++ Calculator/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Calculator
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite a operação desejada:");
+            Console.WriteLine("");
+            Console.WriteLine("(+) Soma     (-) Subtração    (*) Multiplicação    (/) Divisão");
+            Console.WriteLine("");
+            Console.WriteLine("                     (p) Parar o Programa");
+            Console.WriteLine("");
+
+            char operacao = char.Parse(Console.ReadLine());
+
+            switch (operacao)
+            {
+                case '+': Soma(); break;
+
+                case '-': Subtracao(); break;
+
+                case '*': Multiplicacao(); break;
+
+                case '/': Divisao(); break;
+
+                case 'p': System.Environment.Exit(0); break;
+
+                default: Menu(); break;
+            }
+
+        }
+        static void Soma()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro Valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo Valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O Resultado da soma é:{v1 + v2}");
+            Console.ReadKey();
+            Menu();
+        }
+        static void Subtracao()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro Valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo Valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O Resultado da subtração é:{v1 - v2}");
+            Console.ReadKey();
+            Menu();
+        }
+        static void Multiplicacao()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro Valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo Valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O Resultado da multiplicação é:{v1 * v2}");
+            Console.ReadKey();
+            Menu();
+        }
+        static void Divisao()
+        {
+            Console.Clear();
+            Console.WriteLine("Primeiro Valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo Valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O Resultado da divisão é:{v1 / v2}");
+            Console.ReadKey();
+            Menu();
+        }
+    }
+}
---
+++ MeuApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System; //importações
+
+namespace MeuApp //namespace
+{
+    internal class Program //class 
+    {
+         static void Main(string[] args) //método principal (Main)
+        {
+        }
+    }
+}
+
