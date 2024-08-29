b8b8b750153cce17d5f0e06f60676a27f9dd27fa
Nathan de Souza Miranda <nathansm2005@gmail.com>
2024-08-29 17:10:41 -0300

    Teste commit Stopwatch

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -1,11 +1,33 @@
-﻿
-namespace StopWatch
+﻿namespace StopWatch
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Digite o número de segundos que deseja cronometrar:");
+            int time = int.Parse(Console.ReadLine());
+            Start(time);
+        }
+
+        static void Menu()
         {
 
+        }
+
+        static void Start(int time)
+        {          
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
         }
     }
 }
