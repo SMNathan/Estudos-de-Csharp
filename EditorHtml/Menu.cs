0d80bbd9c8fbb0ddc8419db34a51e998f3a1eaf0
Nathan de Souza Miranda <nathansm2005@gmail.com>
2024-12-04 12:07:37 -0300

    Editor Html Completo

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -10,6 +10,7 @@ namespace EditorHtml {
             WriteOptions();
 
             var option = short.Parse(Console.ReadLine());
+            HandleMenuOption(option);
             
         }
 
@@ -58,6 +59,23 @@ namespace EditorHtml {
             Console.WriteLine("0 - Sair");
             Console.SetCursorPosition(3,10);
             Console.Write("Opção: ");
+        }
+
+        public static void HandleMenuOption(short option) {
+            
+            Console.SetCursorPosition(14,11);
+
+            switch(option) {
+
+                case 1: Editor.Show(); break;
+                case 2: Console.WriteLine("View"); break;
+                case 0: {
+                    Console.Clear();
+                    Environment.Exit(0);
+                    break;
+                }
+                default: Show(); break;
+            }
         }
     }
 }
