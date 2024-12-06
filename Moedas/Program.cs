a8d1d7dad78be04bfe2add217cf030c51f6f7473
Nathan de Souza Miranda <nathansm2005@gmail.com>
2024-12-06 15:38:30 -0300

    C# moedas

---
+++ Moedas/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+namespace Moedas {
+    
+    class Program {
+
+        static void Main() {
+
+            Console.Clear();
+
+            decimal valor = 10.25m;
+            Console.WriteLine(Math.Floor(valor));
+        }
+    }
+}
