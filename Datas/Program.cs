bce69c091903ecd64b1b770edc7bccb2f472e666
Nathan de Souza Miranda <nathansm2005@gmail.com>
2024-12-06 11:25:33 -0300

    teste datas em c#

---
+++ Datas/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace Datas {   
+
+    class Program {
+        static void Main() {
+            
+            Console.Clear();
+            
+            var timeSpan = new TimeSpan();
+            Console.WriteLine(timeSpan);
+            
+            var timeSpanNano = new TimeSpan(5, 12, 8);
+            Console.WriteLine(timeSpanNano);
+
+
+        }
+    }
+}
