5150e7113816d39574363b815d3566488c4706c7
Nathan de Souza Miranda <nathansm2005@gmail.com>
2024-12-17 18:24:47 -0300

    Conclusão Projeto Guiado C#

--- Index/Program.cs
+++ Index/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+string spanOpen = "<span>";
+string spanClosing = "</span>";
+
+int quantityOpen = input.IndexOf(spanOpen);
+int quantityClosing = input.IndexOf(spanClosing);
+quantityOpen += spanOpen.Length;
+int quantityLenght = quantityClosing - quantityOpen; 
+quantity = input.Substring(quantityOpen, quantityLenght);
+
+string div = "<div>"; 
+output = input.Substring(div.Length);
+int divClosing = output.IndexOf("</div>");
+output = output.Remove(divClosing, div.Length + 1);
+output = output.Replace("&trade", "&reg");
+
+Console.WriteLine("Quantity: " + quantity);
+Console.WriteLine("Output: " + output);
